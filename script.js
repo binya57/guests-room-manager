f2ab32a3ea84f6211d4c0ac6da2b2de5421094d7
binya <binya57+dev@gmail.com>
2024-03-29 15:59:12 +0300

    finish search functionallity, add classes, change language

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const app = document.getElementById('app');
+//@ts-check
+const app = /**@type {HTMLDivElement}*/ (document.getElementById('app'));
 const roomsContainer = document.createElement('div');
+roomsContainer.className = 'rooms-container'
 const addRoomButton = document.createElement('button');
 const searchGuestInput = document.createElement('input');
-
+searchGuestInput.placeholder = 'חפש אורח';
+searchGuestInput.type = 'search';
 /**
  * @typedef Room
  * @property {string} name
@@ -22,6 +25,9 @@ function addRoom() {
         guests: []
     };
     rooms.push(newRoom);
+    if (!searchGuestInput.isConnected) {
+        app.insertBefore(searchGuestInput, roomsContainer);
+    }
     saveRooms();
     renderRoom(newRoom);
 }
@@ -36,76 +42,86 @@ function renderRoom(room) {
     const roomGuestsEl = document.createElement('div');
 
     roomEl.id = getRoomElementId(room.id);
+    roomEl.className = 'room';
+    roomTitleEl.className = 'title';
+    roomGuestsEl.className = 'guests'
 
     if (room.name) {
-        // roomTitleEl.innerText = room.name;        
         const roomNameEl = document.createElement('span');
         const editButton = document.createElement('button');
-        editButton.innerText = 'edit';
+        editButton.innerText = 'ערוך';
         editButton.onclick = e => {
-            roomTitleEl.innerHTML = '';
+            clearElementContent(roomTitleEl);
             const inputEl = document.createElement('input');
             inputEl.value = room.name;
+            inputEl.placeholder = 'שם החדר';
             const acceptNameButton = document.createElement('button');
             acceptNameButton.innerText = 'V'
             acceptNameButton.onclick = e => {
-                const existingRoom = rooms.find(r => r.id === room.id);
-                existingRoom.name = inputEl.value;
-                roomTitleEl.innerHTML = '';
+                room.name = inputEl.value;
+                clearElementContent(roomTitleEl);
                 roomTitleEl.innerText = inputEl.value;
                 saveRooms();
-                renderRoom(existingRoom);
+                renderRoom(room);
             };
             roomTitleEl.append(inputEl, acceptNameButton);
         }
         const deleteButton = document.createElement('button');
-        deleteButton.innerText = 'delete';
+        deleteButton.innerText = 'מחק חדר';
         deleteButton.onclick = e => {
             roomEl.remove();
             rooms.splice(rooms.indexOf(room), 1);
+            if (!rooms.length) {
+                searchGuestInput.remove();
+            }
             saveRooms();
         }
         roomNameEl.innerText = room.name;
         roomTitleEl.append(roomNameEl, editButton, deleteButton)
     } else {
         const inputEl = document.createElement('input');
+        inputEl.placeholder = 'שם החדר';
         const acceptNameButton = document.createElement('button');
         acceptNameButton.innerText = 'V'
         acceptNameButton.onclick = e => {
-            const existingRoom = rooms.find(r => r.id === room.id);
-            existingRoom.name = inputEl.value;
-            roomTitleEl.innerHTML = '';
+            room.name = inputEl.value;
+            clearElementContent(roomTitleEl)
             roomTitleEl.innerText = inputEl.value;
             saveRooms();
-            renderRoom(existingRoom);
+            renderRoom(room);
         };
         roomTitleEl.append(inputEl, acceptNameButton);
     }
 
     const guestListEl = document.createElement('ul');
-    guestListEl.className = 'guests';
+    guestListEl.className = 'guests-list';
     guestListEl.append(...room.guests.map(guest => {
         const li = document.createElement('li');
-        li.dataset.guest = guest;
-        li.innerText = guest;
+        const text = document.createElement('span');
+        text.dataset.guest = guest;
+        text.innerText = guest;
+        li.append(text);
         return li;
     }))
     const guestNameInput = document.createElement('input');
+    guestNameInput.placeholder = 'שם האורח';
     const addGuestButton = document.createElement('button');
     addGuestButton.onclick = e => {
         if (!guestNameInput.value) return;
         room.guests.push(guestNameInput.value);
         guestNameInput.value = '';
-        guestListEl.innerHTML = '';
+        clearElementContent(guestListEl);
         guestListEl.append(...room.guests.map(guest => {
             const li = document.createElement('li');
-            li.dataset.guest = guest;
-            li.innerText = guest;
+            const text = document.createElement('span');
+            text.dataset.guest = guest;
+            text.innerText = guest;
+            li.append(text);
             return li;
         }))
         saveRooms();
     }
-    addGuestButton.innerText = 'Add Guest';
+    addGuestButton.innerText = 'הוסף אורח';
     roomGuestsEl.append(guestListEl, guestNameInput, addGuestButton);
 
     roomEl.append(roomTitleEl, roomGuestsEl);
@@ -141,38 +157,57 @@ function loadRooms() {
     return JSON.parse(saved);
 }
 
-addRoomButton.innerText = 'Add Room'
+addRoomButton.innerText = 'הוסף חדר'
 addRoomButton.onclick = e => addRoom();
 searchGuestInput.addEventListener('input', e => {
-    const { value } = e.target;
+    const { value } = /**@type {HTMLInputElement}*/(e.target);
     if (!value) {
         rooms.forEach(renderRoom);
         return;
     }
-    const filteredRooms = rooms.filter(r => r.guests.some(g => g.includes(value)));
-    filteredRooms.forEach(room => {
-        const guestsList = document.getElementById(getRoomElementId(room.id)).querySelector('.guests');
-        guestsList.childNodes.forEach(guestNode => {
-            const clone = guestNode.cloneNode(false);
-            if (clone.dataset.guest.includes(value)) {
-                const split = clone.dataset.guest.split(value).map(s => {
-                    const span = document.createElement('span');
-                    span.innerText = s;
-                    return span;
-                });
+    rooms.forEach(room => {
+        const guestsList = document.getElementById(getRoomElementId(room.id))?.querySelector('.guests-list');
+        if (!guestsList) throw new Error('Could not find guests list');
+        guestsList.querySelectorAll('li span[data-guest]').forEach(guestTextElement => {
+            if (!(guestTextElement instanceof HTMLElement)) throw new Error();
+            const guestName = guestTextElement.dataset.guest ?? '';
+            if (!guestName.includes(value)) {
+                clearElementContent(guestTextElement);
+                guestTextElement.innerText = guestName;
+                return;
+            }
+            if (guestName === value) {
+                clearElementContent(guestTextElement);
                 const strong = document.createElement('strong');
                 strong.innerText = value;
-                clone.appendChild(split[0]);
-                clone.appendChild(strong);
-                clone.appendChild(split[1]);
-
-            } else {
-                const text = document.createTextNode(clone.dataset.guest);
-                clone.appendChild(text);
+                guestTextElement.appendChild(strong);
+                return;
             }
-            guestNode.replaceWith(clone);
+            const split = guestName.split(value).map(s => {
+                if (!s) { // is a match
+                    const strong = document.createElement('strong');
+                    strong.innerText = value;
+                    return strong;
+                }
+                const span = document.createElement('span');
+                span.innerText = s;
+                return span;
+            });
+            clearElementContent(guestTextElement);
+            guestTextElement.append(...split);
         })
     })
 })
-app.append(searchGuestInput, roomsContainer, addRoomButton)
+
+/**
+ * 
+ * @param {HTMLElement} element 
+ */
+function clearElementContent(element) {
+    while (element.lastChild) {
+        element.removeChild(element.lastChild);
+    }
+}
+
+app.append(rooms.length ? searchGuestInput : '', addRoomButton, roomsContainer)
 
